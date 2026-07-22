3074768eda5c5d8f245e091aa8766f8628b57a93
Alo2aa <Alo2aa@gmail.com>
2026-07-22 14:21:45 +0300

    Update database.js

--- Graduation-Ideas/server/database.js
+++ Graduation-Ideas/server/database.js
@@ -1,10 +1,12 @@
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const dbPath = path.join(__dirname, "data", "ideas.db");
 
-const db = new sqlite3.Database("./ideas.db", (err) => {
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
-    console.log(err.message);
+    console.error("Database Connection Error: ", err.message);
   } else {
-    console.log("Database Connected");
+    console.log("Database Connected successfully inside Volume");
   }
 });
 
