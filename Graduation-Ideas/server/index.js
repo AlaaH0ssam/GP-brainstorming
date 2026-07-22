fcc85c5737e8af85e3b4aa9bfe330bbe8bbff146
Alo2aa <Alo2aa@gmail.com>
2026-07-22 14:14:40 +0300

    Update index.js

--- Graduation-Ideas/server/index.js
+++ Graduation-Ideas/server/index.js
@@ -95,13 +95,13 @@ app.patch("/ideas/:id/vote", (req, res) => {
     }
   );
 });
-app.use(express.static(path.join(__dirname, "../client/dist")));
+/*app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
-
+*/
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}...`);
